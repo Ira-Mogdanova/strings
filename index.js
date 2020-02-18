4d93b28c8aa0eb40c4c075d43e9f27413bc4b5ef
Ira-Mogdanova <60886569+Ira-Mogdanova@users.noreply.github.com>
2020-02-18 19:45:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 var btn = document.querySelector('button');
 var input = document.getElementById("input"); 
-var check = document.getElementById('check');
+var check = document.getElementById("check");
 var output = document.getElementById("output");
+var btn = document.querySelector("button");
 var inputVal = input.value;
+var outputVal = output.value;
 var checkVal = check.value;
 
-function reverse(s){
-  check.style.visibility = 'hidden';
-  output.value = '';
-  for(var i = s.length -1; i>=0; i--){
-    output.value = output.value +=s[i];
-  }
+
+function reverse(s,t) {
+  t = '';
+    for (var i = s.length - 1; i >= 0; i--){
+        t = t += s[i];
+    }
+    console.log(t);
 }
 
-function endWith(s){
-  output.value = '';  
-  if((checkVal == s[s.length - 1]) && (checkVal.length == 1)){
-    output.value = 'true';
-  }  
-  else {
-    output.value = 'false';
+function endWith(s,t,c){
+  t = '';
+  var n = 0;
+  var d = '';
+  for(var i = (s.length - c.length); i < s.length; i++){
+    for(var j = 0; j < c.length; j++){
+      if(s[i] == c[j]) n++;
+    }
   }
+  if(n == c.length) {
+    t = true;
+  }
+  else t = false;
+  console.log(t);
 }
 
-function startWith(s){
-  output.value = '';
-  output.value = '';  
-  if((checkVal == s[0]) && (checkVal.length == 1)){
-    output.value = 'true';
-  }  
-  else {
-    output.value = 'false';
+function startWith(s,t,c){
+  t = '';
+  var n = 0;
+  for(var i = 0; i < c.length; i++){
+    if(s[i] == c[i]) n++;
   }
+  if(n == c.length) {
+    t = true;
+  }
+  else t = false;
+  console.log(t);
 }
 
-function isCamelCase(s){
-  output.value = '';  
+function isCamelCase(s,t){
+  t = '';  
   var j = 0, k = 0, n =0;
   for(var i = 0; i < s.length; i++){
     if((s[i] == s[i].toUpperCase()) && (s[i] != ' '))
@@ -47,12 +58,14 @@ function isCamelCase(s){
         n++;
   }
   if((n == 0) && (k >= 2) && (j >= 2)){
-    output.value = 'true';
-  } else output.value = 'false';
+    t = true;
+  } else t = false;
+
+  console.log(t);
 }
 
-function isSnakeCase(s){
-  output.value = '';  
+function isSnakeCase(s,t){
+  t = '';  
   var j = 0, k = 0, n =0, l = 0;
 
   for(var i = 0; i < s.length; i++){
@@ -69,61 +82,45 @@ function isSnakeCase(s){
   }
 
   if((n == 0) && (k >= 2) && (j == 0) && (l >= 1)){
-    output.value = 'true';
-  } else output.value = 'false';
+    t = true;
+  } else t = false;
+
+  console.log(t);
 }
 
-function isNan(s){
-  output.value = '';
-  t = '0123456789';
+function isNan(s,t){
+  t = '';
+  d = '0123456789';
   var k = 0;
   for(var i = 0; i < s.length; i++){
-    if(t.search(s[i]) == -1){
+    if(d.search(s[i]) == -1){
       k++;
     }
   }
   if(k == s.length){
-    output.value = 'true';
+    t = true;
   }
   else 
-    output.value = 'false';
+    t = false;
+
+  console.log(t);
 }
 
-function isPhone(s){
-  output.value = '';
-  t = '0123456789';
-  if((s[0] == '+') && (s[4] == '(') && (s[7] == ')') 
-  && (t.search(s[1]) != -1) && (t.search(s[2]) != -1) && (t.search(s[3]) != -1) 
-  && (t.search(s[5]) != -1) && (t.search(s[6]) != -1) && (t.search(s[8]) != -1)
-  && (t.search(s[9]) != -1) && (t.search(s[10]) != -1) && (t.search(s[11]) != -1)
-  && (t.search(s[12]) != -1) && (t.search(s[13]) != -1) && (t.search(s[14]) != -1) && (s.length == 15))  {
-    output.value = 'true';
+function isPhone(s,t){
+  t = '';
+  d = '0123456789';
+  if((s[0] == '+') && (d.search(s[1]) == '3') && (d.search(s[2]) == '7') && (d.search(s[3]) == '5') 
+  && (d.search(s[4]) != -1) && (d.search(s[5]) != -1) && (d.search(s[6]) != -1) 
+  && (d.search(s[7]) != -1) && (d.search(s[8]) != -1) && (d.search(s[9]) != -1) 
+  && (d.search(s[10]) != -1) && (d.search(s[11]) != -1) && (d.search(s[12]) != -1))  {
+    t = true;
   }
   else 
-    output.value = 'false';
+    t = false;
+
+  console.log(t);
 }
 
-function isEmail(s){
-  output.value = ''; 
-  var k = 0, n = 0;
-  var t = "@mail.com"; 
-  for(var i = 0; i < s.length; i++){
-    while(s[i] != '@'){
-      k++;
-    }
-  }
-
-  for(var i = k; i <  s.length; i++){
-    if(t.length == s.length - k){
-      if(s[i] == t[i]){
-        n++;
-      }
-    }
-  }
-  if(n == t.length){
-    output.value = 'true';
-  }
-  else{
-    output.value = 'false';
-  }
+function isEmail(s,t,k){
+ 
 }
