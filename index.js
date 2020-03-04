4d2629b7ac8bf41be987988f9f9b8cb5e2414988
Ira-Mogdanova <60886569+Ira-Mogdanova@users.noreply.github.com>
2020-03-04 20:35:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -119,32 +119,14 @@ function isPhone(s,t){
 
 function isEmail(s,t,c){
   t = '';
-  k = 0;
-  var n = 0;
-  var c = '@mail.com';
-  for(var i = 0; i < s.length-8; i++){
-    if((s[i] == ' ') || (s[i] == '#') || (s[i] == '?') 
-    || (s[i] == '$') || (s[i] == '%') || (s[i] == '&')
-    || (s[i] == ',') || (s[i] == '"') || (s[i] == '-')
-    || (s[i] == '!') || (s[i] == '~') || (s[i] == '_')
-    || (s[i] == ')') || (s[i] == '$') || (s[i] == ':')
-    || (s[i] == ';') || (s[i] == '/') || (s[i] == '\\')
-    || (s[i] == '№') || (s[i] == '^') || (s[i] == '`')
-    || (s[i] == '(') || (s[i] == '+') || (s[i] == '='))
-      k++;
-  }
-  for(var i = (s.length - 9); i < s.length; i++){
-    for(var j = 0; j < 8; j++){
-      if(s[i] == c[j]) n++;
-    }
-  }
-  if((n == 9) && (k == 0)) {
+  var c = /@mail.com/ig;
+  var t = c.exec(s);
+  if(t!=null) {
     t = true;
   }
-  else 
-    if((k != 0) && (n == 9)) 
-      alert("Enter correct email : emailemail@mail.com");
-    else t = false;
-  
+  else  {
+    t = false;
+    alert("Enter correct email : emailemail@mail.com");
+  }
   return t;
 }
